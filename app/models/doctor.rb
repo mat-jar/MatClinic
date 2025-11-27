24621f7724f287e8ce9719ee39e1d286e6c31b04
Mateusz Jarczyński <mat.jarcz@gmail.com>
2025-11-27 21:56:20 +0100

    Fix validation in Doctors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ApplicationRecord
     has_many :appointments, dependent: :destroy
     
     validates :first_name, :last_name,  presence: true
-    validates :first_name, :last_name, format: { with: /\A\D*\z/, message: "cannot contain any digits" }
+    validates :first_name, :last_name, format: { with: /\A\D*\z/, message: "cannot contain any digits" }, allow_blank: true
 
     def full_name
         "#{first_name} #{last_name}"
